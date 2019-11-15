0d2055bbe314b5cc33cf02e6d0c7d024e35b1536
jorritvanderheide <l3lockbuster@protonmail.com>
2019-11-15 02:06:03 +0100

    Add partial scss, brand image, animation

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 (function($) {
     "use strict";
   
-    // Close mobile menu on click
+    //Close mobile menu on click
     $('.nav-link').click(function() {
       $('.navbar-collapse').collapse('hide');
     });
   
-    // Collapse navbar on scroll
+    //Collapse navbar on scroll
     var navbarCollapse = function() {
       if ($("#navbar").offset().top > 100) {
         $("#navbar").addClass("navbar-scrolled");
@@ -14,9 +14,16 @@
         $("#navbar").removeClass("navbar-scrolled");
       }
     };
-    // Collapse if page not at top
+
+    //Collapse if page not at top
     navbarCollapse();
+
     // Collapse on scroll
     $(window).scroll(navbarCollapse);
+
+    //Fade header text on scroll
+    $(window).scroll(function(){
+      $(".header-text").css("opacity", 1 - $(window).scrollTop() / 85);
+    });
   
-  })(jQuery); 
+  })(jQuery);
