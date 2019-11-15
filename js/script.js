7b77168858762db8e7b2e5be611b1f62e5d2bb4a
jorritvanderheide <l3lockbuster@protonmail.com>
2019-11-15 13:00:14 +0100

    Add html pages, footer

--- js/script.js
+++ js/script.js
@@ -1,29 +1,32 @@
-(function($) {
-    "use strict";
-  
-    //Close mobile menu on click
-    $('.nav-link').click(function() {
-      $('.navbar-collapse').collapse('hide');
-    });
-  
-    //Collapse navbar on scroll
-    var navbarCollapse = function() {
-      if ($("#navbar").offset().top > 100) {
-        $("#navbar").addClass("navbar-scrolled");
-      } else {
-        $("#navbar").removeClass("navbar-scrolled");
-      }
-    };
+(function ($) {
+  "use strict";
 
-    //Collapse if page not at top
-    navbarCollapse();
-
-    // Collapse on scroll
-    $(window).scroll(navbarCollapse);
-
-    //Fade header text on scroll
-    $(window).scroll(function(){
-      $(".header-text").css("opacity", 1 - $(window).scrollTop() / 85);
-    });
+  //Scroll to top
   
-  })(jQuery);
+
+  //Close mobile menu on click
+  $('.nav-link').click(function () {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  //Collapse navbar on scroll
+  var navbarCollapse = function () {
+    if ($("#navbar").offset().top > 100) {
+      $("#navbar").addClass("navbar-scrolled");
+    } else {
+      $("#navbar").removeClass("navbar-scrolled");
+    }
+  };
+
+  //Collapse if page not at top
+  navbarCollapse();
+
+  //Collapse on scroll
+  $(window).scroll(navbarCollapse);
+
+  //Fade header text on scroll
+  $(window).scroll(function () {
+    $(".header-text").css("opacity", 1 - $(window).scrollTop() / 85);
+  });
+
+})(jQuery);
