0a6159921d49837c08fa1061a3e16d5170fb0d78
jorritvanderheide <l3lockbuster@protonmail.com>
2019-11-29 10:38:56 +0100

    Make prettier

--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
-(function ($) {
+(function($) {
   "use strict";
 
   //Close mobile menu on click
-  $('.nav-link').click(function () {
-    $('.navbar-collapse').collapse('hide');
+  $(".nav-link").click(function() {
+    $(".navbar-collapse").collapse("hide");
   });
 
   //Collapse navbar on scroll
-  var navbarCollapse = function () {
-    if ($(".mainNav").offset().top > ( ($(".header").outerHeight()) - ($(".mainNav").outerHeight()) - ( ($(window).width() / 40) + ($(window).scrollTop() / 3.5 ) ) ) ){
+  var navbarCollapse = function() {
+    if (
+      $(".mainNav").offset().top >
+      $(".header").outerHeight() -
+        $(".mainNav").outerHeight() -
+        ($(window).width() / 40 + $(window).scrollTop() / 3.5)
+    ) {
       $(".mainNav").addClass("navbar-scrolled");
     } else {
       $(".mainNav").removeClass("navbar-scrolled");
@@ -22,19 +27,24 @@
   $(window).scroll(navbarCollapse);
 
   //Fade header text on scroll
-  $(window).scroll(function () {
-    var x = window.matchMedia("(max-width: 992px)")
+  $(window).scroll(function() {
+    var x = window.matchMedia("(max-width: 992px)");
     // $(".header-text").css("padding-bottom", (20 - $(window).scrollTop() / 10) + "%");
 
     if (x.matches) {
-      $(".sectionbox").css("margin-top", (-5.5 - $(window).scrollTop() / 50) + "%");
+      $(".sectionbox").css(
+        "margin-top",
+        -5.5 - $(window).scrollTop() / 50 + "%"
+      );
       $(".headerrow").css("opacity", 1 - $(window).scrollTop() / 400);
     } else {
-      $(".sectionbox").css("margin-top", (-2.5 - $(window).scrollTop() / 50) + "%");
+      $(".sectionbox").css(
+        "margin-top",
+        -2.5 - $(window).scrollTop() / 50 + "%"
+      );
       $(".headerrow").css("opacity", 1 - $(window).scrollTop() / 150);
     }
   });
 
   //Swipe navigation?
-
 })(jQuery);
