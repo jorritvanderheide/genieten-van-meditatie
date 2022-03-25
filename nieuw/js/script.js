f1d749b78bc9d09b2c67ac866e44cadf4478c953
l3lockbuster <l3lockbuster@protonmail.com>
2022-03-25 13:32:59 +0100

    Update cookie functionality

--- nieuw/js/script.js
+++ nieuw/js/script.js
@@ -19,29 +19,6 @@ window.onscroll = function () {
   navbarCollapse();
 };
 
-// reCAPTCHA
-function onSubmit(token) {
-  if (document.getElementById("newsletter-email").value.length == 0) {
-    alert("Vul eerst uw email adres in");
-    window.location.reload();
-  } else {
-    document.getElementById("newsletter").submit();
-  }
-}
-
-function onSubmitContact(token) {
-  if (
-    document.getElementById("contact-form-email").value.length == 0 ||
-    document.getElementById("contact-form-phone").value.length == 0 ||
-    document.getElementById("contact-form-message").value.length == 0
-  ) {
-    alert("Vul eerst alle velden in");
-    window.location.reload();
-  } else {
-    document.getElementById("contact-form").submit();
-  }
-}
-
 function displayMenu() {
   if (document.getElementById("mobilenav").classList.contains("collapse")) {
     document.getElementById("mobilenav").classList.remove("collapse");
